09b57a1cbd8684ba4c9b82f96831d3fabc1f82e6
Alberto Sierra <alberto.sierralozano@gmail.com>
2017-07-29 18:34:21 -0400

    adding generate_metadata_path and get_metadata_of_scene

--- lib/metadata_lib/metadata_utils.py
+++ lib/metadata_lib/metadata_utils.py
@@ -7,6 +7,16 @@ import maya.cmds as cmds
 import metadata
 
 
+def generate_metadata_path(scene_path):
+    scene_name = os.path.basename(scene_path)
+    scene_folder = os.path.dirname(scene_path)
+    return os.path.normpath(
+            os.path.join(scene_folder, "metadata", 
+                        "{}.metadata".format(scene_name.split(".")[0])
+                        )
+            )
+
+
 def make_metadata_from_local(metadata_file):
     if not is_metadata_file(metadata_file):
         raise Exception("{} must be a valid Metadata File".format(os.path.basename(metadata_file)))
@@ -32,6 +42,13 @@ def generate_metadata_from_scene(save=True):
         metadata_file = local_metadata.save_local_metadata()
         return local_metadata, metadata_file
     return local_metadata, None
+
+
+def get_metadata_of_scene():
+    scene_path = cmds.file(sn=True, q=True)
+    metadata_path = generate_metadata_path(scene_path)
+    return make_metadata_from_local(metadata_path)
+
 
 
 def open_maya_file(metadata_file):
