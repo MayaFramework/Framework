0ed3502b6d6496b77404ce02e0fc47545525986c
Unknown <antonioromero.est@gmail.com>
2018-04-14 15:21:16 +0200

    test

--- plugins/playblaster/playbasterCheck.py
+++ plugins/playblaster/playbasterCheck.py
@@ -3,6 +3,7 @@ import playblasterUI as playblasterUI
 import os
 
 
+
 def checkWindowAtStartMaya():
     if cmds.panel('blasterCam', exists=True) or cmds.workspaceControl('playblaster', exists=True):
         global playblasterValues
