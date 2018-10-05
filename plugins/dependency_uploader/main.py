68fb962d05c005d1df8a1c66009251b32d1bc7d6
Unknown <miguel.molledo.alvarez@gmail.com>
2018-10-05 18:49:44 +0200

    updated main file

--- plugins/dependency_uploader/main.py
+++ plugins/dependency_uploader/main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     from uploader_window import UploaderWindow
     app = QtWidgets.QApplication(sys.argv)
     widget = UploaderWindow()
-    widget.ASK_TO_PUBLISH = False
+    widget.ASK_TO_PUBLISH = True
     widget.PUBLISH_TO_CHK = True
     widget.PUBLISH_TO_OUT = True
     obj = gui_loader.get_default_container(widget, "UPLOADER")
